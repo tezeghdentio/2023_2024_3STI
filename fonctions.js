e9d35094839cb26ae3c53e781b6f8e83d655e8e4
tezeghdentio <95339020+tezeghdentio@users.noreply.github.com>
2023-11-07 15:10:04 +0100

    valid

--- fonctions.js
+++ fonctions.js
@@ -1,28 +1,32 @@
 function verifTxt(ch)
 {
-ch=ch.toUpperCase();
-i=0;
-test=true;
+    ch=ch.toUpperCase();
+    i=0;
+    test=true;
+    while( (i<ch.length) && (test==true) )
+    {
+        if(((ch.charAt(i)<"A") || (ch.charAt(i)>"Z")) && (ch.charAt(i)!=" "))
+        {
+            test=false;
+        }
 
-while( (i<ch.length) && (test==true) )
-{
-if(((ch.charAt(i)<"A") || (ch.charAt(i)>"Z")) && (ch.charAt(i)!=" "))
-{
-test=false;
+    }
+
+    return(test);
 }
-i++;
-alert(i);
-alert(test);
-}
-return(test);
-}
+
 function verif()
 {
-nom=document.getElementById("nom").value;
-prenom=document.getElementById("prenom").value;
+    nom=document.getElementById("nom").value;
+    prenom=document.getElementById("prenom").value;
 
-if((verifTxt(nom)==false) ||(verifTxt(prenom)==false) )
-{
-alert("verifier votre nom ou prenom");
-}
+    if((verifTxt(nom)==false) ||(verifTxt(prenom)==false) )
+    {   
+        alert("verifier votre nom ou prenom");
+    }
+    else if((document.getElementById("homme").checked==false)&&(document.getElementById("femme").checked==false))
+            {
+                alert("veuillez choisir un sexe");
+            }
+   
 }
